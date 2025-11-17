8bbbe074995c91dacbecdbcb84732369c9e9eb3a
mohamedalaaaz <88798693+mohamedalaaaz@users.noreply.github.com>
2025-11-17 02:07:22 +0200

    Add files via upload

--- programs/mycalclouter/src/lib.rs
+++ programs/mycalclouter/src/lib.rs
@@ -6,21 +6,25 @@ declare_id!("96Df3fCBXacgGpqZZWXgxu2YNMPK6aJbCFQqBoj7RKk5");
 pub mod mycalclouter {
     use super::*;
 
-    pub fn create(ctx:context<Create>,init_value:String)->programresult{
-        let claclouter=&mut ctx.accounts.claclouter;
-        claclouter.greeting=init_value;
+    pub fn create(ctx: Context<Create>, init_value: String) -> Result<()> {
+        let claclouter = &mut ctx.accounts.claclouter;
+        claclouter.greeting = init_value;
         Ok(())
-
     }
-
-    
 }
 
 #[derive(Accounts)]
 pub struct Create<'info> {
-    #[account(init, payer=user, space=264)]
+    #[account(init, payer = user, space = 264)]
     pub claclouter: Account<'info, Claclouter>,
     #[account(mut)]
     pub user: Signer<'info>,
     pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct Claclouter {
+    pub greeting: String,
+    pub result: i64,
+    pub remainder: i64,
 }
